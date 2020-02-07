5cddf7e81c3afe8357d3d70866d8b84b7d95f775
myharlay <harlay@sina.com>
2020-02-07 16:33:11 +0800

    new app, orm

--- www/app.py
+++ www/app.py
@@ -1,19 +1,21 @@
-import logging; logging.basicConfig(level=logging.INFO)
+import logging;
 
-import asyncio, os, json,time
-from datetime import datatime
-
+logging.basicConfig(level=logging.INFO)
+import asyncio
 from aiohttp import web
 
-def index(request):
-    return web.Response(boby=b'<h1>Good job</h1>', headers={'content-type':'text/html'})
-async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET','/',index)
-    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
-    logging.info('server started at http://17/0/0/1:9000...')
-    return srv
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+async def index(request):
+    return web.Response(body=b'<h1>Good job</h1>', content_type='text/html')
+
+
+def init():
+    app = web.Application()
+    app.router.add_get('/', index)
+    web.run_app(app, host='127.0.0.1', port=9000)
+
+
+if __name__ == "__main__":
+    init()
+
+
